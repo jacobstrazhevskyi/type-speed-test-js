fb4b8091fbc8a30442a7886e79931bc3f3d56ad7
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2023-01-26 20:35:59 +0200

    added-eng-glossary-and-formated-him

--- src/index.js
+++ src/index.js
@@ -6,30 +6,51 @@
 
 // eslint-disable-next-line import/extensions
 import ruGlossary from './glossaries/ruGlossary.js';
+// eslint-disable-next-line import/extensions
+import engGlossary from './glossaries/engGlossary.js';
+
+// const ruUnicodeCodes = [];
+const engUnicodeCodes = [];
+
+// for (let i = 1040; i <= 1103; i++) {
+//     ruUnicodeCodes.push(i);
+// }
+// ruUnicodeCodes.push(1025, 1105);
+
+for (let i = 65; i <= 90; i++) {
+    engUnicodeCodes.push(i);
+}
+for (let i = 97; i <= 122; i++) {
+    engUnicodeCodes.push(i);
+}
 
 let languageNow = ruGlossary;
 
 const languages = {
     rus: ruGlossary,
+    eng: engGlossary,
 };
 
 const selectLanguage = document.getElementById('select-language');
 const wordsDisplay = document.getElementById('wordsDisplay');
 
 selectLanguage.onchange = function() {
-    languageNow = languages[`${selectLanguage.value}`].split('\n');
-    if (selectLanguage.value === 'rus') {
-        languageNow.shift();
+    languageNow = languages[`${selectLanguage.value}`].split(/\t|\n/g);
+    languageNow.shift();
+    if (selectLanguage.value === 'eng') {
+        languageNow = languageNow.filter((elem) => engUnicodeCodes.includes(elem.charCodeAt()));
     }
     console.log(languageNow);
 };
 
-document.addEventListener('keypress', listener);
-
-function start() {
-
-}
-
 function listener(evt) {
     console.log(evt.key);
 }
+
+function start() {
+    document.removeEventListener('keypress', start);
+    console.log('removed');
+    document.addEventListener('keypress', listener);
+}
+
+document.addEventListener('keypress', start);
