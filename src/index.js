02cb044b86f6bb0d94b0b39a24bcaf9dafa9fadb
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2023-01-27 20:20:57 +0200

    added-more-elements

--- src/index.js
+++ src/index.js
@@ -9,39 +9,10 @@ import ruGlossary from './glossaries/ruGlossary.js';
 // eslint-disable-next-line import/extensions
 import engGlossary from './glossaries/engGlossary.js';
 
-// const ruUnicodeCodes = [];
-const engUnicodeCodes = [];
-
-// for (let i = 1040; i <= 1103; i++) {
-//     ruUnicodeCodes.push(i);
-// }
-// ruUnicodeCodes.push(1025, 1105);
-
-for (let i = 65; i <= 90; i++) {
-    engUnicodeCodes.push(i);
-}
-for (let i = 97; i <= 122; i++) {
-    engUnicodeCodes.push(i);
-}
-
-let languageNow = ruGlossary;
-
-const languages = {
-    rus: ruGlossary,
-    eng: engGlossary,
-};
-
-const selectLanguage = document.getElementById('select-language');
+let time = 60;
+let intervalId;
+const timerBar = document.getElementById('timer');
 const wordsDisplay = document.getElementById('wordsDisplay');
-
-selectLanguage.onchange = function() {
-    languageNow = languages[`${selectLanguage.value}`].split(/\t|\n/g);
-    languageNow.shift();
-    if (selectLanguage.value === 'eng') {
-        languageNow = languageNow.filter((elem) => engUnicodeCodes.includes(elem.charCodeAt()));
-    }
-    console.log(languageNow);
-};
 
 function listener(evt) {
     console.log(evt.key);
@@ -50,7 +21,66 @@ function listener(evt) {
 function start() {
     document.removeEventListener('keypress', start);
     console.log('removed');
-    document.addEventListener('keypress', listener);
+    document.addEventListener('keydown', listener);
+    time = 60;
+    timerBar.value = 60;
+    // eslint-disable-next-line no-use-before-define
+    intervalId = setInterval(startCount, 1000);
 }
+
+function tick() {
+    if (time === 0) {
+        clearInterval(intervalId);
+        console.log('Time is out!');
+        document.removeEventListener('keypress', listener);
+        document.addEventListener('keypress', start);
+    }
+    if (time > 0) {
+        time--;
+        timerBar.value--;
+        console.log(time);
+    }
+}
+
+function startCount() {
+    tick();
+}
+
+const engUnicodeCodes = [];
+
+for (let i = 65; i <= 90; i++) {
+    engUnicodeCodes.push(i);
+}
+for (let i = 97; i <= 122; i++) {
+    engUnicodeCodes.push(i);
+}
+
+let languageNow = ruGlossary.split(/\t|\n/g);
+languageNow.shift();
+
+const languages = {
+    rus: ruGlossary,
+    eng: engGlossary,
+};
+
+const selectLanguage = document.getElementById('select-language');
+
+for (let i = 0; i < 100; i++) {
+    const element = document.createElement('span');
+    element.textContent = `${languageNow[Math.floor(Math.random() * languageNow.length)]} `;
+    wordsDisplay.appendChild(element);
+}
+
+selectLanguage.onchange = function() {
+    languageNow = languages[`${selectLanguage.value}`].split(/\t|\n/g);
+    languageNow.shift();
+    if (selectLanguage.value === 'eng') {
+        languageNow = languageNow.filter((elem) => engUnicodeCodes.includes(elem.charCodeAt()));
+        languageNow = languageNow.join(' ').split(' ');
+        languageNow = languageNow.filter((elem) => engUnicodeCodes.includes(elem.charCodeAt()));
+        languageNow = languageNow.filter((elem) => !/[*()-]/g.test(elem.split('')));
+    }
+    console.log(languageNow);
+};
 
 document.addEventListener('keypress', start);
