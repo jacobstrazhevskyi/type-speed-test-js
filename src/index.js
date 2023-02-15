5e8f2ec37e58544d2b4ee3464875802b4a5418e8
Jacob Strazhevskyi <jacob.strazhevskyi@gmail.com>
2023-02-15 13:53:31 +0200

    added-result-tablet

--- src/index.js
+++ src/index.js
@@ -16,9 +16,22 @@ let intervalId;
 const timerBar = document.getElementById('timer');
 const wordsDisplay = document.getElementById('wordsDisplay');
 const input = document.getElementById('input');
+
+const closeTabletButton = document.querySelector('.close-button');
+const overTablet = document.querySelector('.over-tablet');
+const screenBlocker = document.querySelector('.fullscreen-blocker');
+
+closeTabletButton.onclick = () => {
+    overTablet.style.display = 'none';
+    screenBlocker.style.display = 'none';
+};
+
 let marg = document.body.clientWidth / 2;
 
 input.style.left = `${(document.body.clientWidth / 2) + 6}px`;
+
+overTablet.style.left = `${(document.body.clientWidth / 2) - 250}px`;
+overTablet.style.top = `${document.body.clientHeight / 2}px`;
 
 const engUnicodeCodes = [];
 
@@ -43,11 +56,41 @@ let wordNowLetterIndex = 0;
 let wordWrited = false;
 let writtenWord = '';
 
+let wpm = 0;
+let cpm = 0;
+
 const ruKeys = [70, 188, 68, 85, 76, 84, 192, 186, 80, 66, 81, 82, 75, 86, 89, 74, 71, 72, 67, 78, 69, 65, 219, 87, 88, 73, 79, 221, 83, 77, 222, 190, 90, 189];
 
 setInterval(() => {
     input.value = '';
 }, 1);
+
+let languageNow = ruGlossary.split(/\t|\n/g);
+languageNow.shift();
+
+const languages = {
+    rus: ruGlossary,
+    eng: engGlossary,
+};
+
+function generateWordArray() {
+    for (let i = 0; i < 100; i++) {
+        const element = document.createElement('span');
+        const word = `${languageNow[Math.floor(Math.random() * languageNow.length)]} `.split('');
+        for (let j = 0; j < word.length; j++) {
+            const wordSpan = document.createElement('span');
+            wordSpan.textContent = word[j];
+            element.appendChild(wordSpan);
+        }
+        wordsDisplay.appendChild(element);
+    }
+}
+
+function clearWordDisplay() {
+    for (let i = 1; i < wordsDisplay.children.length; i++) {
+        wordsDisplay.children[i].remove();
+    }
+}
 
 function listener(evt) {
     let wordNow = '';
@@ -72,6 +115,7 @@ function listener(evt) {
                 wordsDisplay.children[wordNowIndex].children[wordNowLetterIndex].setAttribute('style', 'opacity: 0.1;');
                 wordNowLetterIndex++;
                 marg -= 11;
+                cpm++;
                 writtenWord += evt.key;
             } else {
                 wordsDisplay.children[wordNowIndex].children[wordNowLetterIndex].setAttribute('style', 'color: red;');
@@ -85,6 +129,7 @@ function listener(evt) {
             if (wordsDisplay.children[wordNowIndex].children[wordNowLetterIndex].textContent === evt.key) {
                 wordsDisplay.children[wordNowIndex].children[wordNowLetterIndex].setAttribute('style', 'opacity: 0.1;');
                 wordNowLetterIndex++;
+                cpm++;
                 marg -= 11;
                 writtenWord += evt.key;
             } else {
@@ -96,11 +141,10 @@ function listener(evt) {
         }
     }
     
-    console.log(wordNow);
-    console.log(evt.keyCode);
-    
-    console.log(writtenWord);
-    
+    if (writtenWord === wordNow) {
+        wpm++;
+    }
+
     if (writtenWord.length === wordNow.length) {
         wordWrited = true;
     }
@@ -112,15 +156,22 @@ function listener(evt) {
         wordNowLetterIndex = 0;
     }
     wordsDisplay.children[1].setAttribute('style', `margin-left:${marg}px;`);
+
+    console.log( 
+        {
+            wpm, cpm, wordNow, writtenWord,
+        },
+    );
 }
 
 function start() {
     document.removeEventListener('keypress', start);
     console.log('removed');
     document.addEventListener('keydown', listener);
-    time = 60;
+    time = 2;
     timerBar.value = 60;
     wordsDisplay.children[1].setAttribute('style', 'margin-left: 50%;');
+    marg = document.body.clientWidth / 2;
     // eslint-disable-next-line no-use-before-define
     intervalId = setInterval(startCount, 1000);
 }
@@ -131,6 +182,13 @@ function tick() {
         console.log('Time is out!');
         document.removeEventListener('keypress', listener);
         document.addEventListener('keypress', start);
+        wordsDisplay.children[1].setAttribute('style', 'transition: all 1s ease-in-out; margin-left: 50%;');
+        overTablet.style.display = 'flex';
+        wordNowIndex = 1;
+        wordNowLetterIndex = 0;
+        wordWrited = false;
+        writtenWord = '';
+        clearWordDisplay();
     }
     if (time > 0) {
         time--;
@@ -141,27 +199,6 @@ function tick() {
 
 function startCount() {
     tick();
-}
-
-let languageNow = ruGlossary.split(/\t|\n/g);
-languageNow.shift();
-
-const languages = {
-    rus: ruGlossary,
-    eng: engGlossary,
-};
-
-function generateWordArray() {
-    for (let i = 0; i < 100; i++) {
-        const element = document.createElement('span');
-        const word = `${languageNow[Math.floor(Math.random() * languageNow.length)]} `.split('');
-        for (let j = 0; j < word.length; j++) {
-            const wordSpan = document.createElement('span');
-            wordSpan.textContent = word[j];
-            element.appendChild(wordSpan);
-        }
-        wordsDisplay.appendChild(element);
-    }
 }
 
 generateWordArray();
@@ -182,6 +219,7 @@ selectLanguage.onchange = () => {
     while (wordsDisplay.children.length !== 1) {
         wordsDisplay.children[wordsDisplay.children.length - 1].remove();
     }
+    clearWordDisplay();
     generateWordArray();
     wordsDisplay.children[1].setAttribute('style', 'margin-left: 50%;');
     marg = document.body.clientWidth / 2;
